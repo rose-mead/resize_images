395746225a4c69fc207819726e6ce06acac31c40
Rose-Mead <rmkmead@hotmail.com>
2021-02-16 11:31:01 +1300

    update paths

--- image_shrink.py
+++ image_shrink.py
@@ -1,12 +1,15 @@
 import os.path
 import glob
 from PIL import Image
+import sys
 
-SRC_DIR = '/Users/rosemead/Developer/Personal_Projects/Em_Trent_images/images'
-TARG_DIR = '/Users/rosemead/Developer/Personal_Projects/Em_Trent_images/resized_images'
+DIR  = str(sys.argv[1])
+
+SRC_DIR = f'/root/{DIR}'
+TARG_DIR = f'/root/{DIR}/resized_images'
 
 # File needs to contain a '-' and be a jpg
-GLOB_PARMS = "[0-9]*-[0-9]*.jpg"
+GLOB_PARMS = "*-*.jpg"
 
 def create_folder ():
     if not os.path.exists(TARG_DIR):
@@ -15,12 +18,11 @@ def create_folder ():
 def save_resized_image ():
     for file in glob.glob(os.path.join(SRC_DIR, GLOB_PARMS)):
         if file not in glob.glob(os.path.join(TARG_DIR, GLOB_PARMS)):
-            print(file)
             img = Image.open(file)
            
             file_name = os.path.split(file)[1]
 
-            file_path = os.path.join( TARG_DIR, file_name) 
+            file_path = os.path.join(TARG_DIR, file_name) 
             img.save(file_path, quality=50)
 
         else:
@@ -28,7 +30,6 @@ def save_resized_image ():
                 file,os.path.join(os.path.split(TARG_DIR)[-2:])))
 
 
-create_folder()
 save_resized_image()
 
 
